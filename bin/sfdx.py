f50432a46f4791af078bb9bef8808bc34ce070c4
kacperosko <kacper.osko@icloud.com>
2023-03-19 19:39:08 +0100

    Improve SFDX commands

--- bin/sfdx.py
+++ bin/sfdx.py
@@ -15,8 +15,8 @@ class SalesforceCommands:
 
     def __init__(self, username: str, password: str, security_token: str, domain: str = None):
         """
-        Parse login credential to Org You want to clean from vlocity versions. Domain is optional but
-        You have to parse 'test' if You want to authorize sandbox Org
+        Provide login credentials to the Org that You want to authorize. Domain is optional, but
+        You have to provide 'test' if You want to authorize sandbox Org.
 
         :param username:
         :param password:
@@ -61,43 +61,54 @@ class SalesforceCommands:
 
     def delete_bulk(self, s_object: str, dataframe: pd_frame.DataFrame = None, path: str = "generate_from_temp",
                     batch_size: int = 10000, use_serial: bool = True) -> bool:
+        """
+        Function require sObject type which should be deleted and data with record's ID to delete (Dataframe or path to CSV file).
+        If source is not provided, function will take CSV from get_bulk function for provided sObject.
 
+        :param s_object:
+        :param dataframe:
+        :param path:
+        :param batch_size:
+        :param use_serial:
+        :return:
+        """
         if dataframe is None:
             if path == "generate_from_temp":
                 path = os.path.join(TEMP_CSV_DIR, f"{s_object}_get_bulk.csv")
-            elif not os.path.isfile(path):
+            if not os.path.isfile(path):
                 clr.print_error(f">> There is no file in the given path {path}")
                 return False
-            clr.print_info(">> Running bulk delete from CSV file")
+            clr.print_info(f">> Running bulk delete {s_object} from CSV file")
             try:
                 dataframe = pd.read_csv(filepath_or_buffer=path)
             except pd_errors.EmptyDataError:
-                clr.print_error(">> Temp CSV file is empty")
+                clr.print_error(">> CSV file is empty")
                 return False
             except Exception as e:
                 clr.print_error(f">> {e}")
                 return False
         else:
-            clr.print_info(">> Running bulk delete from Pandas DataFrame")
+            clr.print_info(f">> Running bulk delete {s_object} from Pandas DataFrame")
 
         data = dataframe[['Id']].to_dict('records')
-        data.append({'Id': '0013H00000XYAX8123'})
-        print(data)
+        clr.print_info(f">> {len(data)} records prepared to delete")
 
-        # def delete_bulk(self, s_object, data, batch_size, use_serial):
         try:
             result = self.sf.bulk.__getattr__(s_object).delete(data, batch_size=batch_size, use_serial=use_serial)
         except Exception as e:
             clr.print_error(f">> {e}")
-            return
+            return False
+
         success, failure = 0, 0
         for res in result:
             if res['success']:
                 success += 1
             else:
                 failure += 1
-        clr.print_info(f">> Succesfully deleted {success} records" + " {failure} failed" if failure > 0 else "")
+
+        clr.print_info(f">> Succesfully deleted {success} records, {failure} records failed")
         clr.print_success(">> Deleting ended succesful")
+
         return True
 
 
